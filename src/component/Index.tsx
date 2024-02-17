a0beccc4bf031cee7c38cdcef14e138c01ca7dda
themks001 <mksthe88@gmail.com>
2024-02-17 20:27:00 +0545

    new changes

--- src/component/Index.tsx
+++ src/component/Index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 interface IndexProps {
   index: number;
+  id:string;
   topic: string;
 }
 
-const Index: React.FC<IndexProps> = ({ index, topic }) => {
+const Index: React.FC<IndexProps> = ({ index, topic, id }) => {
   return (
     <>
       <div className="content-container" id="child-grid-1">
-        <div className="content-index">
+        <div className="content-index" id={id}>
           <strong>{index}</strong>
         </div>
         <h2 className="content-heading">
